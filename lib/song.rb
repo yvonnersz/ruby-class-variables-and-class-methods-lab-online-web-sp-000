63c97a1dd74d8e15c8c9ed77972ee5c0ac67d55e
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-03 20:05:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
     hash = {} #hash[genre][count]
     @@genre.each do |genre,count|
       count = 0
-      if hash == nil
+      if hash[genre].uniq!
         hash[genre] = count
       else
         hash[genre] = count +=1
